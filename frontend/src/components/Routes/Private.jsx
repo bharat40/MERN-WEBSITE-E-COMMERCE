7ffb1b3cecf4d5451492499d69614b9c5d1ac7d5
bharat <bharatdhiman40@gmail.com>
2025-08-03 18:40:05 +0530

    private route logic improved

--- frontend/src/components/Routes/Private.jsx
+++ frontend/src/components/Routes/Private.jsx
@@ -8,7 +8,6 @@ export const PrivateRoute = () => {
     const navigate = useNavigate();
     const [authorize, setAuthorize] = useState(false);
     const [auth, setAuth] = useAuth();
-    const [count, setCount] = useState(5);
     useEffect(() => {
         const authCheck = async () => {
             const response = await axios.get(`${import.meta.env.VITE_API}/api/v1/auth/user-auth`, {
@@ -16,22 +15,23 @@ export const PrivateRoute = () => {
                     "Authorization": auth?.token
                 }
             });
-            if (response.data.success) {
+            if (response.data.success === true) {
                 setAuthorize(true);
             }
             else {
                 setAuthorize(false);
+                toast.error("You are not authorized to access dashboard");
+                navigate('/login');
             }
         }
         if (auth?.token) {
             authCheck();
         }
-        const interval = setInterval(() => {
-            setCount((prevValue) => --prevValue);
-        }, 1000);
-        count === 0 && toast.error("You are not authorized, Please login!") && navigate('/login');
-        return () => clearInterval(interval);
-    }, [auth?.token, count, navigate]);
+        else {
+            toast.error("You are not authorized to access dashboard");
+            navigate('/login');
+        }
+    }, [auth?.token]);
 
     return authorize ? <Outlet /> : (<div className="text-center flex justify-center items-center font-bold text-3xl h-[620px]">Verifying your access... Please wait...</div>);
 };
